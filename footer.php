7e75c46a0333eebddbc7e06b5d747217d937b040
Fiona Siah Pei Shi <fionasiah_23@hotmail.com>
2018-02-04 17:56:08 +0800

    Commit Job Table

--- footer.php
+++ footer.php
@@ -13,4 +13,97 @@
 <script src="assets/js/material-dashboard.js?v=1.2.0"></script>
 <!-- Custom JS -->
 <script type="text/javascript" src="assets/js/custom.js"></script>
+<!-- DataTables JavaScript -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+
+<!----------------------- Scripts -------------------------->
+<script>
+
+    (function ($) {
+        $.fn.dataTableExt.oApi.fnGetColumnData = function (dtSet, dtCol, dtUnique, dtFilt, dtEmpty) {
+            // check for column id, no col id >> set up new array
+            if (typeof dtCol === "undefined")
+                return new Array();
+
+            /* true = Filter out duplicates */
+            if (typeof dtUnique === "undefined")
+                dtUnique = true;
+
+            // true = look only at filtered data
+            if (typeof dtFilt === "undefined")
+                dtFilt = true;
+
+            // true = exclude empty values
+            if (typeof dtEmpty === "undefined")
+                dtEmpty = true;
+
+            //Define Var for looping through - a whole list of rows datas
+            var aiRow;
+
+            // Display filtered data
+            if (dtFilt === true)
+                aiRow = dtSet.aiDisplay;
+            // Display all row data
+            else
+                aiRow = dtSet.aiDisplayMaster;
+
+            // Create new array for data 
+            var asFinalData = new Array();
+
+            for (var i = 0, c = aiRow.length; i < c; i++) {
+                iRow = aiRow[i];
+                var aData = this.fnGetData(iRow);
+                var sValue = aData[dtCol];
+
+                // skip the empty values retrieved
+                if (dtEmpty === true && sValue.length === 0)
+                    continue;
+
+                // skip the value if it is in the array
+                else if (dtUnique === true && jQuery.inArray(sValue, asFinalData) > -1)
+                    continue;
+
+                // else append value int the array
+                else
+                    asFinalData.push(sValue);
+            }
+
+            return asFinalData;
+        };
+    }(jQuery));
+
+    /* Function to Create the options */
+    function fnCreateOption(arrayData)
+    {
+        var r = '<select><option value=""></option>', i, iLen = arrayData.length;
+        for (i = 0; i < iLen; i++)
+        {
+            r += '<option value="' + arrayData[i] + '">' + arrayData[i] + '</option>';
+        }
+        return r + '</select>';
+    }
+
+
+    $(document).ready(function () {
+        var dtTable = $('#tblJobs').dataTable({
+            "oLanguage": {
+                "sSearch": "Search all columns:"
+            }
+        });
+
+        /* Specifying while column to put filter ddl */
+        /* fnGetColumnData = Return array of values from the wanted column */
+        var indexOfMyCol = 5; //6th column
+        $("#ddlJobs th").each(function (i) {
+            if (i === indexOfMyCol) {
+                this.innerHTML = fnCreateOption(dtTable.fnGetColumnData(i));
+                $('select', this).change(function () {
+                    dtTable.fnFilter($(this).val(), i);
+                });
+            }
+        });
+    });
+</script>
 </html>
--- jobs.php
+++ jobs.php
@@ -30,110 +30,60 @@ include('header.php');
             <div class="container-fluid">
                 <div class="row">
                     <form method="POST">
-                        <h4>Select the monthly statement to be viewed:</h4>
+                        <h4>Available Jobs:</h4>
                         <hr>
-                            <div class="col-md-3 col-sm-4 col-xs-6">
-                                test
-                            </div>
-                        <?php// } ?>
-                    </form>
-                </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <!--Table-->
+                            <table id="tblJobs" class="table table-bordered table-hover">
+                                <thead>
+                                    <tr id="ddlJobs">
+                                        <th rowspan="2">Urgency</th>
+                                        <th rowspan="2">Engineer Name</th>
+                                        <th rowspan="2">Location</th>
+                                        <th rowspan="2">Job Number</th>
+                                        <th rowspan="2">Traveling Time</th>
+                                        
+                                        <!--For filter bar-->
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <th>Fault Type</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Urgent</td>
+                                        <td>System Architect</td>
+                                        <td>Edinburgh</td>
+                                        <td>61</td>
+                                        <td>Tiger Nixon</td>
+                                        <td>1</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Urgent</td>
+                                        <td>Garrett Winters</td>
+                                        <td>Accountant</td>
+                                        <td>Tokyo</td>
+                                        <td>63</td>
+                                        <td>3</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Urgent</td>
+                                        <td>Junior Technical Author</td>
+                                        <td>San Francisco</td>
+                                        <td>66</td>
+                                        <td>2009/01/12</td>
+                                        <td>5</td>
+                                    </tr>
 
-                <div id="statement_table" class="table-responsive">
-                    <table class="monthlystatement table table-bordered table-condensed" style="max-width: initial;">
-                        <thead>
-                            <tr>
-                                <th id='electricity'>
-                                    <h5 class="textLabel"><i class="material-icons">flash_on</i> ELECTRICITY</h5>
-                                </th>
-                                <th id='water'>
-                                    <h5 class="textLabel"><i class="material-icons">invert_colors</i> WATER</h5>
-                                </th>
-                                <th id='gas'>
-                                    <h5 class="textLabel"><i class="material-icons">whatshot</i> GAS</h5>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="rowTest">
-                                <td>
-                                    <b>Reading taken on <?php echo date_format($readdate, "d/m/Y") ?></b><br>
-                                    <b>Usage:</b> <?php echo "$eusage kWh" ?><br>
-                                    <b>Rate:</b> <?php echo "$$erate/kWh" ?><br>
-                                </td>
-                                <td>
-                                    <b>Reading taken on <?php echo date_format($readdate, "d/m/Y") ?></b><br>
-                                    <b>Usage:</b> <?php echo "$wusage CuM" ?><br>
-                                    <b>Rate:</b> <?php echo "$$wrate/CuM" ?><br>
-                                </td>
-                                <td>
-                                    <b>Reading taken on <?php echo date_format($readdate, "d/m/Y") ?></b><br>
-                                    <b>Usage:</b> <?php echo "$gusage kWh" ?><br>
-                                    <b>Rate:</b> <?php echo "$$grate/kWh" ?><br>
-                                </td>
-                            </tr>
-                            <tr class="rowTest" id='price'>
-                                <td>
-                                    <?php echo "$" . round($eprice, 2) ?>
-                                </td>
-                                <td>
-                                    <?php echo "$" . round($wprice, 2) ?>
-                                </td>
-                                <td>
-                                    <?php echo "$" . round($gprice, 2) ?>
-                                </td>
-                            </tr>
-                            <tr class="rowTest">
-                                <td colspan='3'>
-                                    <h6><u><b>Additional Charges:</b></u></h6>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <dl>
-                                                <dt><?php echo "Waterborne Fee: $$WFTotal" ?></dt>
-                                                <dd><?php echo "- $WFUsage CuM @ $$WFRate/CuM" ?><dd>
-                                                <dt><?php echo "Water Conservation Tax: $$WCTotal" ?></dt> 
-                                                <dd><?php echo "- $WCRate% of water bill" ?></dd>
-                                            </dl>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <dl>
-                                                <dt><?php echo "Sanitary Appliance Fee: $$SATotal" ?></dt>
-                                                <dd><?php echo "- $SAUsage fittings @ $$SARate/fitting" ?></dd>
-                                                <dt><?php echo "Refuse Removal <small>(by SembWaste Pte Ltd)</small>: $$RRTotal" ?></dt>
-                                                <dd><?php echo "- $RRUsage Quantity @ $$RRRate/quantity" ?></dd>
-                                            </dl>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            <?php $AmtDue = round($eprice + $wprice + $gprice + $WFTotal + $WCTotal + $SATotal + $RRTotal, 2); ?>
-                            <tr>
-                                <td colspan='3'>
-                                    <span class="total"><?php echo "Total Amount Payable (including 7% GST): $$AmtDue" ?></span>
-                                    <?php if (isset($_SESSION['paid'])) { ?>
-                                        <button disabled class='btn pull-right'><i class="material-icons">check_circle</i><b> Fully Paid</b></button>
-                                    <?php } else { ?>
-                                        <a href='MakePayment.php?sid=<?php echo $SID ?>&total=<?php echo $AmtDue ?>&month=<?php echo $currmonth ?>&year=<?php echo $curryear ?>'><button class='button pull-right'><b>Pay Bill</b> <i class="fa fa-arrow-right" aria-hidden="true"></i></button></a>
-                                    <?php } ?>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    <footer class="footer">
-        <div class="container-fluid">
-            <p class="copyright pull-right">
-                &copy;
-                <script>
-                    document.write(new Date().getFullYear())
-                </script>
-                The SWEG way of life.
-            </p>
-        </div>
-    </footer>
 </div>
 
 <?php 
